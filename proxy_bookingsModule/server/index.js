e717a87f7417d70c01cf76d5684af7c33308d1fd
fradleybrancis <fradleybrancis@gmail.com>
2019-05-01 19:19:12 -0700

    change request routes to only make requests to reviews module

--- proxy_bookingsModule/server/index.js
+++ proxy_bookingsModule/server/index.js
@@ -1,48 +1,19 @@
+require("newrelic");
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => res.redirect(`/bookings/${Math.floor(Math.random()* 100)}`));
-
-app.use('/bookings/:roomid', express.static(path.join(__dirname, '../public')));
-
-app.get('/photosandcomments/:accommodationid', (req, res) => {
-  axios.get(`http://localhost:3001/photosandcomments/${req.params.accommodationid}`)
-    .then(response => {
-      res.send(response.data);
-    });
-});
-
-app.get('/bookings/:accommodationid/reserve', (req, res) => {
-  axios.get(`http://localhost:3003/bookings/${req.params.accommodationid}/reserve`)
-    .then(response => {
-      res.send(response.data);
-    });
-});
-
-app.get('/bookings/:accommodationid/reserve/:startDate&:endDate', (req, res) => {
-  axios.get(`http://localhost:3003/bookings/${req.params.accommodationid}/reserve/${req.params.startDate}&${req.params.endDate}`)
-    .then(response => {
-      res.send(response.data);
-    });
-});
+// app.get('/abodes/:abode_id/reviews', (req, res) => {
 
 app.get('/abodes/:abode_id/reviews', (req, res) => {
   axios.get(`http://localhost:3002/abodes/${req.params.abode_id}/reviews`)
     .then(response => {
-      res.send(response.data);
-    });
-});
-
-app.get('/homes', (req, res) => {
-  axios.get(`http://localhost:3004/homes`)
-    .then(response => {
+      res.status(200);
       res.send(response.data);
     });
 });
